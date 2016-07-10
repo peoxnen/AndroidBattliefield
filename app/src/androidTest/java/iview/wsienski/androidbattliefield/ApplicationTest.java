3d319e921d22c89a90fd52811cb6d875602a57c3
peoxnen <sienski.witold@gmail.com>
2016-07-10 11:00:23 +0200

    Initial commit

---
+++ app/src/androidTest/java/iview/wsienski/androidbattliefield/ApplicationTest.java
@@ -0,0 +1,13 @@
+package iview.wsienski.androidbattliefield;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
