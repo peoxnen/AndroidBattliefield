05c009a6ba7c997dbf0958fbf48eaf9fefb21a2e
peoxnen <sienski.witold@gmail.com>
2016-07-10 11:52:24 +0200

    butterknife, IntentServce

--- app/src/main/java/iview/wsienski/androidbattliefield/MainActivity.java
+++ app/src/main/java/iview/wsienski/androidbattliefield/MainActivity.java
@@ -1,7 +1,12 @@
 package iview.wsienski.androidbattliefield;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_startService)
+    public void pressBtn(View view) {
+        Intent intent = new Intent(this, MsgService.class);
+        intent.putExtra(MsgService.EXTRA_MSG, getString(R.string.btn_resp));
+        startService(intent);
     }
 }
